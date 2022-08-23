b572411139d1134eebffb2805c8ea5bb96d15047
dnlclmnt <dnlclmnt@gmail.com>
2022-08-22 19:28:27 -0700

    finished functionality

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@ import React, { useState } from 'react'
 import Square from './components/Square'
 import './App.css'
 
+// Player 1 wins XXX
+// Player 2 wins XXX
+// Player wins on last move XXX
+// Alert only pops up on next user click, even after winner is decided.
+
+
+
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [player, setPlayer] = useState(1)
   const [counter, setCounter] = useState(0)
+  // newSquares array will be used to update squares state on each player move.
+  let newSquares = [...squares] 
 
   const handleGame = (index) => {
+
     if (calculateWinner(squares) === "X") {
       alert("Winner is Player One")
-      return;
+      return
     } else if (calculateWinner(squares) === "O") {
       alert("Winner is Player Two")
       return
     }
 
-    let newSquares = [...squares] 
-      if(counter === squares.length - 1){
-        alert("Game Over")
-      }
-      console.log(counter)
-    
+    // Counter variable will be compared against length of squares array - 1 to assess if any more moves can be made.
+    if(counter === squares.length) {
+      alert("Game Over")
+    }
 
+      // Player 1 functionality. Switches player state to 2 after turn is over. Adds to counter state if box is clicked. Updates newSquares array at index to "X" and updates squares state with the newSquares array.
       if (player === 1) {
         if (newSquares[index] === "X") {
           alert("You have already marked this square")
@@ -31,11 +40,11 @@ const App = () => {
         } else {
           newSquares[index] = "X"
           setSquares(newSquares)
-          setCounter(counter+1)
+          setCounter(counter + 1)
           setPlayer(2)
         }
       }
-
+      // Player 2 functionality. Switches player state to 1 after turn is over. Adds to counter state if box is clicked. Updates newSquares array at index to "O" and updates squares state with the newSquares array.
       if (player === 2) {
         if (newSquares[index] === "O") {
           alert("You have already marked this square")
@@ -49,12 +58,8 @@ const App = () => {
         }
       }
 
-      
-
-    
-
   }
-
+    // Functionality to define a winner (a certain player connects three in a row).
   function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
@@ -74,12 +79,13 @@ const App = () => {
     }
     return null;
   }
-
+    // Reset button functionality. Resets all states back to their original state.
     const handleReset = () => {
       setPlayer(1)
       setCounter(0)
       setSquares(squares.fill(null))
     }
+
 
   return (
     <>
@@ -87,11 +93,11 @@ const App = () => {
       <div className="gameboard">
       {squares.map((value, index) => {
         return (
-          <Square calculateWinner={calculateWinner} squares={squares} handleGame={handleGame} value={value} index={index}/>
+          <Square squares={squares} handleGame={handleGame} value={value} index={index}/>
         )
       })}
       </div>
-     
+    <p>Player: {player}</p>
     <div className="button">
     <button onClick ={handleReset}>Reset</button> 
     </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -3,10 +3,7 @@ import React from 'react'
 const Square = (props) => {
 
   const handleClick = () => {
-   
-
     props.handleGame(props.index)
-
   }
 
   return (
